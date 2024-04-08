923595f9ddf9121da31fa93d55d9b6cc56042737
koubek <koubek.jenda@icloud.com>
2024-04-08 08:50:01 +0200

    added enemy value

--- src/players/strategies/BattleStrategy.java
+++ src/players/strategies/BattleStrategy.java
@@ -4,11 +4,16 @@ import players.Player;
 
 public abstract class BattleStrategy {
     protected Player parentPlayer;
+    protected Player enemy;
 
     public abstract int getAttackStrategy();
     public abstract int getDefendStrategy();
 
     public void setParentPlayer(Player parentPlayer) {
         this.parentPlayer = parentPlayer;
+    }
+
+    public void setEnemy(Player enemy) {
+        this.enemy = enemy;
     }
 }
