330e71fdd00a3d7fcaa456c35e349b51610212b3
koubek <koubek.jenda@icloud.com>
2024-04-08 07:55:46 +0200

    added runGame() method

--- src/game/Program.java
+++ src/game/Program.java
@@ -1,4 +1,14 @@
 package game;
 
-public class Program {
+import util.Input;
+
+public abstract class Program {
+    public static void runGame() {
+        int answer;
+        String[] answers;
+
+        System.out.println("Welcome to the Arena!");
+        answers = new String[]{"Warrior", "Scout", "Wizard"};
+        answer = Input.question("Choose your player class", answers);
+    }
 }
