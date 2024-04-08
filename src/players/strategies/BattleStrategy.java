1c55352b52382e715506c5c7958f964e8d62ab42
koubek <koubek.jenda@icloud.com>
2024-04-08 08:44:26 +0200

    removed constructor, added setter for parentPlayer

--- src/players/strategies/BattleStrategy.java
+++ src/players/strategies/BattleStrategy.java
@@ -5,10 +5,10 @@ import players.Player;
 public abstract class BattleStrategy {
     private Player parentPlayer;
 
-    public BattleStrategy(Player parentPlayer) {
-        this.parentPlayer = parentPlayer;
-    }
-
     public abstract int getAttackStrategy();
     public abstract int getDefendStrategy();
+
+    public void setParentPlayer(Player parentPlayer) {
+        this.parentPlayer = parentPlayer;
+    }
 }
